1d630da57edc09d6fb3b30b49c5cbae559e2ddff
Asher <ashish.2017@hotmail.com>
2023-11-03 07:24:38 +0000

    (wip) keypair generation with OS CSPRNG

--- wallet/src/main.rs
+++ wallet/src/main.rs
@@ -1,3 +1,12 @@
+use rand::rngs::OsRng;
+use secp256k1::{PublicKey, Secp256k1, SecretKey};
+
 fn main() {
-    println!("Hello, world!");
+    let (sk, pk) = gen_keypair();
+    dbg!(sk, pk);
+}
+
+fn gen_keypair() -> (SecretKey, PublicKey) {
+    let secp = Secp256k1::new();
+    secp.generate_keypair(&mut OsRng)
 }
